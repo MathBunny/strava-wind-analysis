3e79b03293393b33940757850ff9fd3d3509c31e
MathBunny <horatiulazu@gmail.com>
2018-01-08 20:07:55 -0500

    Add scatter plot for aggregate ride analytics

---
+++ charts/scatterplotchart.js
@@ -0,0 +1,31 @@
+const AbstractChart = require('./abstractchart');
+
+class ScatterPlotChart extends AbstractChart {
+  constructor(dataArr, labelText) {
+    super(dataArr);
+    this.data = {
+      datasets:
+        [{ label: labelText,
+          data: dataArr,
+          fill: false,
+          backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(201, 203, 207, 0.2)'],
+          borderColor:
+          ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)', 'rgb(201, 203, 207)'],
+          borderWidth: 1 }],
+    };
+
+    this.options = {
+      title: {
+        display: true,
+      },
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          position: 'bottom',
+        }],
+      },
+    };
+  }
+}
+
+module.exports = ScatterPlotChart;
--- routes/charts.js
+++ routes/charts.js
@@ -11,6 +11,28 @@ const config = require('../config');
 
 const router = express.Router();
 
+router.get('/get/chart/individual-aggregate-ride-scatterplot', (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.send({ error: 'error: unauthenticated user' });
+  } else {
+    stravadatahandler.getActivitiesList(req.user.accessToken).then((data) => {
+      const dataArr = [];
+      data.forEach((activity) => {
+        const activityObj = {};
+        activityObj.x = activity.distance / 1000;
+        activityObj.y = ((activity.distance * 3.6) / activity.moving_time);
+        dataArr.push(activityObj);
+      });
+      const chart = ChartFactory.getChart('scatterplotchart', dataArr, 'Distance-Speed Scatter Plot');
+      const chartData = {
+        data: chart.getData(),
+        options: chart.getOptions(),
+      };
+      res.send(chartData);
+    });
+  }
+});
+
 router.get('/get/chart/individual-wind-radar', (req, res) => {
   if (!req.isAuthenticated()) {
     res.send({ error: 'error: unauthenticated user' });
